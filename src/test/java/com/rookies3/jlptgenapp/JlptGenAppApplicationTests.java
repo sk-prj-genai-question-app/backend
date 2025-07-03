d2f9d63a74460d93167ca01cda27182c7082fdf9
oyushik <craven_sawdust.9i@icloud.com>
2025-07-03 15:41:21 +0900

    first commit

---
+++ src/test/java/com/rookies3/jlptgenapp/JlptGenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookies3.jlptgenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlptGenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

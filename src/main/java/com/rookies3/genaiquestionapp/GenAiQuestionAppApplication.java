b900b7efb8c9194df926e8aab29459b4e89a086e
oyushik <craven_sawdust.9i@icloud.com>
2025-07-03 16:01:54 +0900

    first commit

---
+++ src/main/java/com/rookies3/genaiquestionapp/GenAiQuestionAppApplication.java
@@ -0,0 +1,13 @@
+package com.rookies3.genaiquestionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAiQuestionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenAiQuestionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rookies3/genaiquestionapp/GenAiQuestionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookies3.genaiquestionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenAiQuestionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

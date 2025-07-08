3dbbc2974ec12a7e8ae2ed5dde3ff67c13bc70d8
oyushik <craven_sawdust.9i@icloud.com>
2025-07-08 13:03:53 +0900

    dev/1.0.1

--- src/test/java/com/rookies3/genaiquestionapp/GenAiQuestionAppApplicationTests.java
+++ src/test/java/com/rookies3/genaiquestionapp/GenAiQuestionAppApplicationTests.java
@@ -1,9 +1,9 @@
 package com.rookies3.genaiquestionapp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@SpringBootTest
+@DataJpaTest
 class GenAiQuestionAppApplicationTests {
 
 	@Test

eea9d3d6bcb2f4e0cb6857679ac9a152eeeecb45
oyushik <craven_sawdust.9i@icloud.com>
2025-07-08 12:13:04 +0900

    dev/1.0.1

--- src/main/java/com/rookies3/genaiquestionapp/GenAiQuestionAppApplication.java
+++ src/main/java/com/rookies3/genaiquestionapp/GenAiQuestionAppApplication.java
@@ -1,5 +1,6 @@
 package com.rookies3.genaiquestionapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,8 @@ import org.springframework.web.client.RestTemplate;
 public class GenAiQuestionAppApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(GenAiQuestionAppApplication.class, args);
 		}
 

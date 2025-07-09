a5414c131cb82a625856eba8dea10c13a3117040
hhye0 <jhy030123@naver.com>
2025-07-09 11:54:13 +0900

    권한 수정

--- src/main/java/com/rookies3/genaiquestionapp/auth/config/SecurityConfig.java
+++ src/main/java/com/rookies3/genaiquestionapp/auth/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
                         // jwt 토큰 없이 접근 가능
                         .requestMatchers("/auth/signup", "/auth/signin", "/auth/refresh","/h2-console/**").permitAll()
                         .requestMatchers("/api/problems/**").permitAll() // 일단 임시 접근 가능
-                        .requestMatchers("/api/answer-record").permitAll()
+                        .requestMatchers("/api/answer-record/**").authenticated()
                         // jwt 토큰이 없으면 접근 불가능
                         .requestMatchers("/auth/me/information", "/auth/signout" ).authenticated()
                         .anyRequest().authenticated()

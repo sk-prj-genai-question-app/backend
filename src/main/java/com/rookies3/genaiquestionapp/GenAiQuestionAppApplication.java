f4af2839d21a834f023d2c400a09bca52528b474
oyushik <craven_sawdust.9i@icloud.com>
2025-07-10 20:52:28 +0900

    hotfix: remove dotenv

--- src/main/java/com/rookies3/genaiquestionapp/GenAiQuestionAppApplication.java
+++ src/main/java/com/rookies3/genaiquestionapp/GenAiQuestionAppApplication.java
@@ -1,6 +1,5 @@
 package com.rookies3.genaiquestionapp;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +11,6 @@ import org.springframework.web.client.RestTemplate;
 public class GenAiQuestionAppApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(GenAiQuestionAppApplication.class, args);
-		}
+	}
 }
